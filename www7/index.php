eaaf134874ec222b6d0a583022fdbf565a14b2cb
mis68 <mis68@medicine.local>
2021-12-13 14:44:43 +0700

    d
    d

--- www7/index.php
+++ www7/index.php
@@ -11,7 +11,7 @@
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <h1 class="title">
-                       ITS MED.CMU STACK
+                       ITS MED.CMU STACK (PHP7.4)
                     </h1>
                     <h2 class="subtitle">
                         Your local development environment
